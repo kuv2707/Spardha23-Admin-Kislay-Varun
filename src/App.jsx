e1a962266b1fc55817322888d2f4a4fc7e8acd71
Kislay <88523649+kuv2707@users.noreply.github.com>
2023-09-27 04:44:25 +0530

    auth context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home/Home'
-import Login from './components/Login/Login'
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import {AuthProvider} from "./contexts/AuthContext";
+import Navbar from "./components/Navbar/Navbar";
+
 
 function App() {
-  
 
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+	return (
+		<AuthProvider>
+			<BrowserRouter>
+      <Navbar></Navbar>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</BrowserRouter>
+		</AuthProvider>
+	);
 }
 
-export default App
+export default App;
