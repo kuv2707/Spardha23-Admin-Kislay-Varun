3107074d927f0435d4f61328bd830f46bfc3d82e
Kislay <88523649+kuv2707@users.noreply.github.com>
2023-09-28 01:12:43 +0530

    finalized

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import { useContext } from "react";
 import styles from "./Navbar.module.css";
 import { AuthContext } from "../../contexts/AuthContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
 	const { logout, isLoggedIn } = useContext(AuthContext);
-
+	const navigate=useNavigate();
+	function logoutHandler() {
+		logout();
+		navigate('/login')
+	}
 	return (
 		<div className={styles.navbar}>
 			{isLoggedIn && (
-				<button className={styles.logOut} onClick={logout}>
+				<button className={styles.logOut} onClick={logoutHandler}>
 					<Link to="/login">Logout</Link>
 				</button>
 			)}
--- src/components/Pages/DocumentVerification.jsx
+++ src/components/Pages/DocumentVerification.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import axios from 'axios';
 
 function DocumentVerification() {
+  const baseUrl = import.meta.env.VITE_BACKEND_URL;
+  const [documents, setDocuments] = useState([])
+  const {token} = useContext(AuthContext)
+  useEffect(function(){
+    axios.get(baseUrl + '/documents/', {
+      headers: {
+        Authorization: `Token ${token}`
+      }
+    }).then(function(response){
+      console.log(response.data)
+      setDocuments(response.data)
+    }).catch(function(error){
+      console.log(error)
+    })
+  },[])
   return (
-    <div>DocumentVerification</div>
-  )
+		<>
+			<h1>Document Verification</h1>
+			{documents.map((document, index) => (
+				<div key={index} className="userCard">
+					{Object.keys(document).map((key, subIndex) => (
+						<div key={subIndex} className="fields">
+							<span className="fieldName">{key}:</span>{" "}
+							{JSON.stringify(document[key])}
+						</div>
+					))}
+				</div>
+			))}
+		</>
+  );
+
 }
 
 export default DocumentVerification
